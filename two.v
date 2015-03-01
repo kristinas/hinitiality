9bcf43f6fe2d8d3301ac8a3c400ef75b10657b8e
Kristina Sojakova <kristinas@cmu.edu>
2015-03-01 18:06:10 -0500

    Almost finished.

--- two.v
+++ two.v
@@ -5,6 +5,25 @@ Local Open Scope fibration_scope.
 
 Definition Bip : Type := { C : Type &  C * C }.
 
+Definition FibBip (X : Bip) : Type :=
+  match X with (C;(c0,c1)) =>
+    { E : C -> Type & E (c0) * E (c1) }
+  end.
+
+Definition bip2fibbip {X : Bip} : Bip -> FibBip X :=
+  match X with (C;(c0,c1)) => fun Y =>
+    match Y with (D;(d0,d1)) =>
+      (fun _ => D; (d0,d1))
+    end
+  end.
+
+Definition fibbip2bip {X : Bip} : FibBip X -> Bip :=
+  match X with (C;(c0,c1)) => fun Y =>
+    match Y with (E;(e0,e1)) =>
+      ({ x : C & E x};((c0;e0),(c1;e1)))
+    end
+  end.
+
 Definition BipMor (X Y : Bip) : Type :=
   match X, Y with (C;(c0,c1)), (D;(d0,d1)) =>
     { f : C -> D & (f c0 = d0) * (f c1 = d1) }
@@ -15,8 +34,7 @@ Definition bipmor2map {X Y : Bip} : BipMor X Y -> X.1 -> Y.1 :=
     match i with (f;_) => f end
   end.
 
-Definition bipidmor {X : Bip} : BipMor X X :=
-  match X with (C;(c0,c1)) => (idmap; (1, 1)) end.
+Definition bipidmor {X : Bip} : BipMor X X := match X with (C;(c0,c1)) => (idmap; (1, 1)) end.
 
 Definition bipcompmor {X Y Z : Bip} : BipMor X Y -> BipMor Y Z -> BipMor X Z :=
   match X, Y, Z with (C;(c0,c1)), (D;(d0,d1)), (E;(e0,e1)) => fun i j =>
@@ -29,183 +47,251 @@ Definition isbipequiv {X Y : Bip} (i : BipMor X Y) : Type :=
   { l : BipMor Y X & bipcompmor i l = bipidmor } *
   { r : BipMor Y X & bipcompmor r i = bipidmor }.
 
-Definition BipEquiv {X Y : Bip} : Type := { i : BipMor X Y & isbipequiv i }.
+Definition BipEquiv (X Y : Bip) : Type := { i : BipMor X Y & isbipequiv i }.
 
-Lemma bipequivEQequiv : forall {X Y : Bip} (i : BipMor X Y),
+Definition BipSec {X : Bip} : forall (Y : FibBip X), Type :=
+  match X with (C;(c0,c1)) => fun Y =>
+    match Y with (E;(e0,e1)) =>
+      { f : forall x, E x & (f c0 = e0) * (f c1 = e1) }
+    end
+  end.
+
+Definition BipSecHot {X : Bip} : forall {Y : FibBip X} (i j : BipSec Y), Type :=
+  match X with (C;(c0,c1)) => fun Y =>
+    match Y with (E;(e0,e1)) => fun i j =>
+      match i, j with (f;(f0,f1)), (g;(g0,g1)) =>
+        { n : f == g & (f0 = n c0 @ g0) * (f1 = n c1 @ g1) }
+      end
+    end
+  end.
+
+Definition BipHot {X Y : Bip} (i j : BipMor X Y) : Type := @BipSecHot X (bip2fibbip Y) i j.
+
+Definition bipidsechot {X : Bip} {Y : FibBip X} : forall {i : BipSec Y}, BipSecHot i i :=
+  match X, Y with (C;(c0,c1)), (D;(d0,d1)) => fun i =>
+    match i with (f;(f0,f1)) =>
+      (fun _ => 1;((concat_1p f0)^,(concat_1p f1)^))
+    end
+  end.
+
+Definition bipidhot {X Y : Bip} {i : BipMor X Y} : BipHot i i := @bipidsechot X (bip2fibbip Y) i.
+
+Definition bipidequiv {X : Bip} : BipEquiv X X :=
+  match X with (C;(c0,c1)) =>
+    (bipidmor;((bipidmor;1),(bipidmor;1)))
+  end.
+
+Lemma bipsecpathEQbipsechot `{Funext} {X} {Y : FibBip X} (i j : BipSec Y) :
+  i = j <~> BipSecHot i j.
+Proof.
+destruct X as [C [c0 c1]], Y as [E [e0 e1]].
+transitivity { n : i.1 = j.1 & i.2 = transport (fun h => (h c0 = e0) * (h c1 = e1)) n^ j.2}.
+  by apply symmetry; apply equiv_path_sigma_contra.
+destruct i as [f [f0 f1]]; destruct j as [g [g0 g1]]; cbn.
+transitivity { n : f = g & (f0 = apD10 n c0 @ g0) * (f1 = apD10 n c1 @ g1)}.
+  apply equiv_functor_sigma_id; induction a; transitivity ((f0 = g0) * (f1 = g1)).
+    by apply symmetry; apply (equiv_path_prod (f0,f1) (g0,g1)).
+  apply equiv_functor_prod'.
+    exact (equiv_concat_r ((concat_1p g0)^) f0).
+  exact (equiv_concat_r ((concat_1p g1)^) f1).
+apply (equiv_functor_sigma' ((equiv_path_forall _ _)^-1)); intro n.
+by apply equiv_idmap.
+Defined.
+
+Definition bipsecpath2bipsechot {X} {Y : FibBip X} (i j : BipSec Y) (u : i = j) :
+  BipSecHot i j := match u with 1 => bipidsechot end.
+
+(** Lemma 3.10 *)
+Global Instance isequiv_bipsecpath2bipsechot `{Funext} {X} {Y : FibBip X} (i j : BipSec Y) :
+  IsEquiv (bipsecpath2bipsechot i j).
+Proof.
+apply (isequiv_homotopic (bipsecpathEQbipsechot i j)); intro u; induction u.
+destruct X as [C [c0 c1]], Y as [E [e0 e1]], i as [f [f0 f1]]; erapply path_sigma.
+  exact 1.
+cbn; unfold functor_prod; apply path_prod; cbn; hott_simpl.
+Qed.
+
+Definition bipmorpath2biphot {X Y : Bip} (i j : BipMor X Y) (u : i = j) : BipHot i j :=
+  @bipsecpath2bipsechot X (bip2fibbip Y) i j u.
+
+(** Lemma 3.6 *)
+Global Instance isequiv_bipmorpath2biphot `{Funext} {X Y} (i j : BipMor X Y) :
+  IsEquiv (bipmorpath2biphot i j).
+Proof. apply (@isequiv_bipsecpath2bipsechot _ X (bip2fibbip Y) i j). Qed.
+
+Definition bipmorpathEQbiphot `{Funext} {X Y} (i j : BipMor X Y) : i = j <~> BipHot i j :=
+  BuildEquiv _ _ (bipmorpath2biphot i j) _.
+
+(** Proposition 3.4 *)
+Lemma bipequivEQequiv `{Funext} {X Y : Bip} (i : BipMor X Y) :
   isbipequiv i <~> IsEquiv (bipmor2map i).
 Proof.
-set (P := (fun X Y => match X, Y with (C;(c_0,c_1)), (D;(d_0,d_1)) => fun i j =>
-  match i, j with (f;(f_0,f_1)), (g;(g_0,g_1)) =>
-  { n : g o f == idmap & (ap g f_0 @ g_0 = n c_0) * (ap g f_1 @ g_1 = n c_1)} end end) :
-  forall X Y (i : BipMor X Y) (j : BipMor Y X), Type).
-assert (equivcompmor : forall {X Y : Bip} (i : BipMor X Y) j,
-(bipcompmor i j = bipidmor) <~> P X Y i j).
-  intros; set (U := X); set (V := Y); destruct X as [C [c0 c1]], Y as [D [d0 d1]].
-  apply transitivity with { n : (bipcompmor i j).1 = (@bipidmor U).1 &
-  (bipcompmor i j).2 = transport (fun h => (h c0 = c0) * (h c1 = c1)) n^ (@bipidmor U).2}.
-    by apply symmetry; refine (equiv_path_sigma' _ (bipcompmor i j) (@bipidmor U)).
-  destruct i as [f [f0 f1]]; destruct j as [g [g0 g1]].
-  apply transitivity with { n : g o f = idmap & (ap g f0 @ g0 = apD10 n c0 @ 1) *
-  (ap g f1 @ g1 = apD10 n c1 @ 1)}.
-    apply equiv_functor_sigma_id; intro n.
-    assert (Ggen : forall (h0 h1 : C -> C) (p : h0 = h1) u0 u1 v0 v1,
-    ((u0, u1) = transport (fun h => (h c0 = c0) * (h c1 = c1)) p^ (v0, v1)) <~> 
-    (u0 = apD10 p c0 @ v0) * (u1 = apD10 p c1 @ v1)). 
-      induction p; intros; simpl; rewrite !concat_1p; apply symmetry.
-      by apply (equiv_path_prod (u0,u1) (v0,v1)).
-    by apply Ggen.
-  apply (equiv_functor_sigma' (equiv_inverse (equiv_path_forall _ _))); intro n.
-  by rewrite !concat_p1; apply equiv_idmap.
 intros; set (U := X); set (V := Y); set (k := i).
 destruct X as [C [c0 c1]], Y as [D [d0 d1]]; destruct i as [f [f0 f1]].
 set (R (x1 x2 y : C) (q : y = x1) (s : y = x2) := { p : _ & q @ p = s}).
-assert (Rcontr : forall x1 x2 y q s, Contr (R x1 x2 y q s)).
-  intros; refine (@contr_equiv' {p : _ & p = q^ @ s} _ _ _).
-  by apply equiv_functor_sigma_id; intro p; apply equiv_moveR_Mp.
 set (S (g : C -> D) x1 x2 y (q : g x2 = y) (s : g x1 = y) := {p : _ & ap g p @ q = s }).
-assert (Scontr : forall g x1 x2 y q s, IsEquiv g -> Contr (S g x1 x2 y q s)).
-  intros; refine (@contr_equiv' {p : _ & p = (ap g)^-1 (s @ q^)} _ _ _).
-  apply equiv_functor_sigma_id; intro p; apply transitivity with (ap g p = s @ q^).
-    by apply symmetry; apply equiv_ap_l.
-  by apply equiv_moveR_pM.
-set (T0 := { l : D -> C & { n : l o f == idmap & { r : _ & { e : f o r == idmap &
-  R _ _ _ (ap l f0) (n c0) * R _ _ _ (ap l f1) (n c1) * S f _ _ _ f0 (e d0) *
-  S f _ _ _ f1 (e d1)}}}}).
-apply transitivity with T0.
-  set (F := (fun x => match x with (((l;(l0,l1));lI),((r;(r0,r1));rI)) =>
-    match (equivcompmor _ _ _ _ lI) with nH =>
-    match (equivcompmor _ _ _ _ rI) with eH =>
-    (l;(nH.1;(r;(eH.1;((l0;fst nH.2),(l1;snd nH.2),(r0;fst eH.2),(r1;snd eH.2))))))
-    end end end): isbipequiv(k) -> T0).
-  set (G := (fun x => match x with (l;(n;(r;(e;((l0;nI0),(l1;nI1),(r0;eI0),(r1;eI1)))))) =>
-    (((l;(l0,l1));(equivcompmor U V k (l;(l0,l1)))^-1 (n;(nI0,nI1))),
-    ((r;(r0,r1));(equivcompmor V U (r;(r0,r1)) k)^-1 (e;(eI0,eI1)))) end) :
-    T0 -> isbipequiv(k)).
-  apply (equiv_adjointify F G).
-    intros [l [n [r [e [[[[l0 nI0] [l1 nI1]] [r0 rI0]] [r1 rI1]]]]]]; simpl.
+transitivity { l : D -> C & { n : l o f == idmap & { r : _ & { e : f o r == idmap &
+R _ _ _ (ap l f0) (n c0) * R _ _ _ (ap l f1) (n c1) * S f _ _ _ f0 (e d0) *
+S f _ _ _ f1 (e d1)} } } }.
+  set (P := (fun X Y => match X, Y with (A;(a0,a1)), (B;(b0,b1)) => fun j k =>
+    match j, k with (g;(g0,g1)), (h;(h0,h1)) =>
+    { n : h o g == idmap & (ap h g0 @ h0 = n a0) * (ap h g1 @ h1 = n a1)} end end) :
+    forall X Y (j : BipMor X Y) (k : BipMor Y X), Type).
+  assert (equivcompmor : forall {X Y : Bip} (j : BipMor X Y) k,
+  (bipcompmor j k = bipidmor) <~> P X Y j k).
+    intros [A [a0 a1]] [B [b0 b1]] [g [g0 g1]] [h [h0 h1]].
+    transitivity { n : h o g == idmap & (ap h g0 @ h0 = n a0 @ 1) * (ap h g1 @ h1 = n a1 @ 1)}.
+      by apply (@bipmorpathEQbiphot _).
+    by apply equiv_functor_sigma_id; intro n; rewrite !concat_p1; apply equiv_idmap.
+  refine (equiv_adjointify _ _ _ _).
+    exact (fun x => match x with (((l;(l0,l1));lI),((r;(r0,r1));rI)) =>
+      match (equivcompmor _ _ _ _ lI) with nH =>
+      match (equivcompmor _ _ _ _ rI) with eH =>
+      (l;(nH.1;(r;(eH.1;((l0;fst nH.2),(l1;snd nH.2),(r0;fst eH.2),(r1;snd eH.2))))))
+      end end end).
+    exact (fun x => match x with (l;(n;(r;(e;((l0;nI0),(l1;nI1),(r0;eI0),(r1;eI1)))))) =>
+      (((l;(l0,l1));(equivcompmor U V k (l;(l0,l1)))^-1 (n;(nI0,nI1))),
+      ((r;(r0,r1));(equivcompmor V U (r;(r0,r1)) k)^-1 (e;(eI0,eI1)))) end).
+    intros [l [n [r [e [[[[l0 nI0] [l1 nI1]] [r0 rI0]] [r1 rI1]]]]]].
     rewrite (eisretr (equivcompmor U V k (l;(l0,l1)))).
     by rewrite (eisretr (equivcompmor V U (r;(r0,r1)) k)); reflexivity.
-  intros [[[l [l0 l1]] lI] [[r [r0 r1]] rI]]; simpl; rewrite !eta_prod; rewrite !eta_sigma.
+  intros [[[l [l0 l1]] lI] [[r [r0 r1]] rI]].
   rewrite (eissect(equivcompmor U V k (l;(l0,l1)))).
   by rewrite (eissect (equivcompmor V U (r;(r0,r1)) k)); reflexivity.
-set (T1 := { l : D -> C & { _ : l o f == idmap & { r : _ & { _ : f o r == idmap & Unit}}}}).
-apply transitivity with T1.
+transitivity { l : D -> C & { _ : l o f == idmap & { r : _ & { _ : f o r == idmap & Unit} } } }.
+  assert (Rcontr : forall x1 x2 y q s, Contr (R x1 x2 y q s)).
+    intros; refine (@contr_equiv' {p : _ & p = q^ @ s} _ _ _).
+    by apply equiv_functor_sigma_id; intro p; apply equiv_moveR_Mp.
+  assert (Scontr : forall g x1 x2 y q s (ge : IsEquiv g), Contr (S g x1 x2 y q s)).
+    intros; refine (@contr_equiv' {p : _ & p = (ap g)^-1 (s @ q^)} _ _ _).
+    apply equiv_functor_sigma_id; intro p; transitivity (ap g p = s @ q^).
+      by apply equiv_moveR_equiv_M.
+    by apply equiv_moveR_pM.
   apply equiv_functor_sigma_id; intro l; apply equiv_functor_sigma_id; intro n.
-  apply equiv_functor_sigma_id; intro r; apply equiv_functor_sigma_id; intro e.
-  by assert (fE := equiv_biinv _ ((l;n),(r;e))); apply @equiv_contr_unit; apply contr_prod.
-apply transitivity with (BiInv f).
-  set (F := (fun x => match x with (l;(n;(r;(e;_)))) => ((l;n),(r;e)) end) : T1 -> BiInv f).
-  set (G := (fun x => match x with ((l;n),(r;e)) => (l;(n;(r;(e;tt)))) end) : BiInv f -> T1).
-  apply (equiv_adjointify F G).
+  apply equiv_functor_sigma_id; intro r; apply equiv_functor_sigma_id; intro e.  
+  assert (fE := isequiv_biinv _ ((l;n),(r;e))); apply @equiv_contr_unit.
+  by apply contr_prod.
+transitivity (BiInv f).
+  refine (equiv_adjointify _ _ _ _).  
+    exact (fun x => match x with (l;(n;(r;(e;_)))) => ((l;n),(r;e)) end).
+    exact (fun x => match x with ((l;n),(r;e)) => (l;(n;(r;(e;tt)))) end).
     by intros [[l n] [r e]]; reflexivity.
-  by intros [l [n [r [e u]]]]; simpl; rewrite (eta_unit u); reflexivity.
-by apply equiv_biinv_equiv.
+  by intros [l [n [r [e u]]]]; rewrite (eta_unit u); reflexivity.
+by apply equiv_biinv_isequiv.
 Qed.
 
+Global Instance isequivprop `{Funext} {X Y : Bip} (i : BipMor X Y) : IsHProp (isbipequiv i).
+Proof. apply (trunc_equiv _ (bipequivEQequiv i)^-1). Qed.
 
-
-
-
-
-
-
-
-
-
-Definition TwoFibAlg (X : TwoAlg) : Type :=
-  match X with (C;(c_0,c_1)) =>
-    { E : C -> Type & E (c_0) * E (c_1) }
-  end.
-
-
-Definition TwoFibHom (X : TwoAlg) : forall (Y : TwoFibAlg X), Type :=
-  match X with (C;(c_0,c_1)) => fun Y =>
-    match Y with (E;(e_0,e_1)) =>
-      { f : forall x, E x & (f c_0 = e_0) * (f c_1 = e_1) }
-    end
-  end.
-
-Definition TwoCell (X Y : TwoAlg) : forall (i j : TwoHom X Y), Type :=
-  match X, Y with (C;(c_0,c_1)), (D;(d_0,d_1)) => fun i j =>
-    match i, j with (f;(p_0,p_1)), (g;(q_0,q_1)) =>
-      TwoFibHom (C;(c_0,c_1)) (fun x => f x = g x; (p_0 @ q_0^, p_1 @ q_1^))
-    end
-  end.
-
-Definition TwoFibCell (X : TwoAlg) : forall (Y : TwoFibAlg X) (i j : TwoFibHom X Y), 
-Type :=
-  match X with (C;(c_0,c_1)) => fun Y =>
-    match Y with (E;(e_0,e_1)) => fun i j =>
-      match i, j with (f;(p_0,p_1)), (g;(q_0,q_1)) =>
-        TwoFibHom (C;(c_0,c_1)) (fun x => f x = g x; (p_0 @ q_0^, p_1 @ q_1^))
-      end
-    end
-  end. 
-
-Definition hasTwoRec (X : TwoAlg) : Type := forall (Y : TwoAlg), TwoHom X Y.
-
-Definition hasTwoInd (X : TwoAlg) : Type := forall (Y : TwoFibAlg X), TwoFibHom X Y.
-
-Definition hasTwoRecUniq (X : TwoAlg) : Type := forall (Y : TwoAlg) (i j : TwoHom X Y),
- i = j.
-
-Definition hasTwoIndUniq (X : TwoAlg) : Type :=
-  forall (Y : TwoFibAlg X) (i j : TwoFibHom X Y), i = j.
-
-Definition twoHinit (X : TwoAlg) : Type := forall (Y : TwoAlg), Contr (TwoHom X Y).
-
-Theorem twoFibHomPathSpace : forall (X : TwoAlg) (Y : TwoFibAlg X) 
-(i j : TwoFibHom X Y),
-  i = j <~> TwoFibCell X Y i j.
+Lemma bippathEQbipequiv `{Univalence} (X Y : Bip) : X = Y <~> BipEquiv X Y.
 Proof.
-intros [C [c_0 c_1]] [E [e_0 e_1]] [f [p_0 p_1]] [g [q_0 q_1]].
-apply transitivity with { a : f = g & transport (fun h => (h c_0 = e_0) * (h c_1 = e_1)) a
-(p_0, p_1) = (q_0, q_1) }.
-  apply symmetry.
-  apply (equiv_path_sigma _ i j).
-
-
-
-
+transitivity { n : X.1 = Y.1 & transport (fun Z => Z * Z) n X.2 = Y.2}.
+  by apply symmetry; erapply equiv_path_sigma.
+assert (H0 : X = Y <~> { n : X.1 = Y.1 & transport (fun Z => Z * Z) n X.2 = Y.2}).
+  by apply symmetry; erapply equiv_path_sigma.
+assert (H1 : { n : X.1 = Y.1 & transport (fun Z => Z * Z) n X.2 = Y.2} <~> BipEquiv X Y).
+destruct X as [C [c0 c1]], Y as [D [d0 d1]]; cbn.
+transitivity { p : C = D & (equiv_path _ _ p c0 = d0) * (equiv_path _ _ p c1 = d1)}.
+  apply equiv_functor_sigma_id; induction a; apply symmetry.
+  by apply (equiv_path_prod (c0,c1) (d0,d1)).
+transitivity {f : C <~> D & (f c0 = d0) * (f c1 = d1)}.
+  by apply (equiv_functor_sigma' (((equiv_path_universe _ _)^-1))); intro f; apply equiv_idmap.
+transitivity {i : BipMor (C;(c0,c1)) (D;(d0,d1)) & IsEquiv (bipmor2map i) }.
+  refine (equiv_adjointify _ _ _ _).
+    exact (fun x => match x with ((BuildEquiv f fE);fM) => ((f;fM);fE) end).
+    exact (fun x => match x with ((f;fM);fE) => ((BuildEquiv _ _ f fE);fM) end).
+    by intros [[f fM] fE]; reflexivity.
+  by intros [[f fE] fM]; reflexivity.
+by apply equiv_functor_sigma_id; intro i; apply symmetry; apply bipequivEQequiv.
 admit.
-  (* apply wAlgFibMapPath_eq_algFibCell'. *)
 Defined.
 
-Theorem twoPathToFibCell (X : TwoAlg) (Y : TwoFibAlg X) (i j : TwoFibHom X Y) :
-  i = j -> TwoFibCell X Y i j.
+Definition bippath2bipequiv (X Y : Bip) (u : X = Y) : BipEquiv X Y :=
+  match u with 1 => bipidequiv end.
+
+(** Theorem 4.8 *)
+Global Instance isequiv_bippath2bipequiv `{Univalence} (X Y : Bip) : IsEquiv (bippath2bipequiv X Y).
 Proof.
-apply equiv_fun; apply twoFibHomPathSpace.
-Defined.
+apply (isequiv_homotopic (bippathEQbipequiv X Y)); intro u; induction u.
+destruct X as [C [c0 c1]]; erapply path_sigma.
+  admit.
+ admit.
+(* exact 1.
+cbn; unfold functor_prod; apply path_prod; cbn; hott_simpl. *)
+Qed.
 
-Theorem twoFibCellToPath (X : TwoAlg) (Y : TwoFibAlg X) (i j : TwoFibHom X Y) :
-  TwoFibCell X Y i j -> i = j.
+Definition isrec (X : Bip) : Type := forall (Y : Bip), BipMor X Y.
+
+Definition isind (X : Bip) : Type := forall (Y : FibBip X), BipSec Y.
+
+Definition hasetacoh (X : Bip) : Type := forall (Y : Bip) (i j : BipMor X Y), BipHot i j.
+
+Definition hasfibetacoh (X : Bip) : Type := forall (Y : FibBip X) (i j : BipSec Y), BipSecHot i j.
+
+Definition ishinit (X : Bip) : Type := forall (Y : Bip), Contr (BipMor X Y).
+
+Definition BipInd : Type := { X : Bip & isind X }.
+
+(** Proposition 4.3 *)
+Lemma isind2hasfibetacoh (X : Bip) : isind X -> hasfibetacoh X.
 Proof.
-exact (twoPathToFibCell X Y i j)^-1.
+destruct X as [C [c0 c1]]; intros Cind [E [e0 e1]] [f [f0 f1]] [g [g0 g1]].
+assert (Hij := Cind (fun x => f x = g x; (f0 @ g0^,f1 @ g1^))).
+exact (Hij.1; ((moveR_pM _ _ _ (fst Hij.2))^, (moveR_pM _ _ _ (snd Hij.2))^)).
 Defined.
 
-Theorem twoHomPathSpace : forall (X Y : TwoAlg) (i j : TwoHom X Y), i = j <~> 
-TwoCell X Y i j.
+(** Proposition 4.5 *)
+Lemma ishinitEQisrechasetacoh `{Funext} (X : Bip) : isrec X * hasetacoh X <~> ishinit X.
 Proof.
-intros [C [c_0 c_1]] [D d] i j; apply (twoFibHomPathSpace (C;(c_0,c_1)) (fun _ => D; d)).
-Defined.
+transitivity (forall Y, BipMor X Y * forall (i j : BipMor X Y), BipHot i j).
+  by apply equiv_prod_coind.
+apply (equiv_functor_forall_id); intro Y; apply symmetry.
+transitivity (BipMor X Y * forall (i j : BipMor X Y), i = j).
+  by apply equiv_contr_inhabited_allpath.
+apply equiv_functor_prod_l; apply equiv_functor_forall_id; intro i.
+by apply equiv_functor_forall_id; intro j; apply bipmorpathEQbiphot.
+Qed.
 
-Theorem twoPathToCell (X Y : TwoAlg) (i j : TwoHom X Y) : i = j -> TwoCell X Y i j.
+Global Instance isindprop `{Funext} (X : Bip) : IsHProp (isind X).
 Proof.
-apply equiv_fun; apply twoHomPathSpace.
-Defined.
+apply hprop_allpath; intros iX iX'; apply path_forall; intro Y.
+by apply (bipsecpath2bipsechot _ _)^-1; apply (isind2hasfibetacoh _ iX).
+Qed.
 
-Theorem twoCellToPath (X Y : TwoAlg) (i j : TwoHom X Y) : TwoCell X Y i j -> i = j.
+(** Theorem 4.6 *)
+Theorem isindEQishinit `{Funext} (X : Bip) : isind X <~> ishinit X.
 Proof.
-exact (twoPathToCell X Y i j)^-1.
-Defined.
+apply equiv_iff_hprop.
+  intro iX; apply (ishinitEQisrechasetacoh).
+  exact (fun Y => iX (bip2fibbip Y), fun Y => isind2hasfibetacoh _ iX (bip2fibbip Y)).
+destruct X as [C [c0 c1]]; intros hX; apply ishinitEQisrechasetacoh in hX; destruct hX as [rC ecC].
+intro Y; destruct (rC (fibbip2bip Y)) as [f [f0 f1]]; destruct Y as [E [e0 e1]].
+destruct (ecC (C;(c0,c1)) (pr1 o f; (f0..1, f1..1)) bipidmor) as [n [n0 n1]].
+split with (fun x => n x # (f x).2); rewrite ((concat_p1 (n c0))^); rewrite ((concat_p1 (n c1))^).
+by rewrite n0^; rewrite n1^; rewrite f0..2; rewrite f1..2; exact (1,1).
+Qed.
 
-Theorem twoIndImpIndUniq : forall (X : TwoAlg), hasTwoInd X -> hasTwoIndUniq X.
+(** Corollary 4.9 *)
+Corollary hinitbipunique `{Univalence} {X Y : Bip} : ishinit X -> ishinit Y -> Contr (X = Y).
 Proof.
-intros [C [c_0 c_1]] h [E [e_0 e_1]] [f [p_0 p_1]] [g [q_0 q_1]]; apply twoFibCellToPath;
- apply h.
-Defined.
+intros hX hY; refine (contr_equiv _ (bippathEQbipequiv X Y)^-1).
+apply (@trunc_sigma _ _ _ (hX Y)); intro i; refine (contr_inhabited_hprop _ _).
+split; split with (fst (equiv_contr_inhabited_allpath (hY X))).
+  by apply (snd (equiv_contr_inhabited_allpath (hX X))).
+by apply (snd (equiv_contr_inhabited_allpath (hY Y))).
+Qed.
 
+(** Corollary 4.7 *)
+Corollary ishinitEQeqtohinit `{Univalence} {X Y : Bip} : ishinit X -> ishinit Y <~> X = Y.
+Proof.
+intro hX; refine (equiv_iff_hprop _ _).
+  by apply equiv_hprop_inhabited_contr^-1; intro u; induction u; apply (hinitbipunique hX hX).
+  by apply (hinitbipunique hX).
+by intro u; induction u; exact hX.
+Qed.
 
-
-
-
+Global Instance bipindprop `{Univalence} : IsHProp BipInd.
+Proof.
+apply hprop_allpath; intros [C iC] [D iD]; apply path_sigma_hprop.
+by apply (hinitbipunique (isindEQishinit _ iC) (isindEQishinit _ iD)).
+Qed.
